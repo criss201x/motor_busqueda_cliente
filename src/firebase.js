c23108c49f8f830963ec9f5a8ef495a4ba7f4b37
cristian <cryteck_1011@hotmail.com>
2021-07-19 21:31:14 -0500

    implementacion google analytics

--- src/firebase.js
+++ src/firebase.js
@@ -9,8 +9,11 @@ var firebaseConfig = {
     projectId: "horarios-y-espacios-fact-tecno",
     storageBucket: "horarios-y-espacios-fact-tecno.appspot.com",
     messagingSenderId: "851789126039",
-    appId: "1:851789126039:web:9594060ded8de271"
+    appId: "1:851789126039:web:9594060ded8de271",
+    measurementId: "G-DWSR3MC0X1"
 };
 // Initialize Firebase
+
 firebase.initializeApp(firebaseConfig);
 export const db = firebase.database().ref("/");
+firebase.analytics();
