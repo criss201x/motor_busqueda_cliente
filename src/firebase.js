ea4f5ccfdb06ebdda03cbc4061747a2993bcaf2a
cristian <cryteck_1011@hotmail.com>
2021-07-19 22:09:23 -0500

    se remueve cache manifiesto y google analytics debido a incompatibilidad con node js

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,5 @@
 import firebase from 'firebase/app'
 import 'firebase/database';
-import 'firebase/analytics';
 
 // Your web app's Firebase configuration
 var firebaseConfig = {
@@ -11,15 +10,8 @@ var firebaseConfig = {
     storageBucket: "horarios-y-espacios-fact-tecno.appspot.com",
     messagingSenderId: "851789126039",
     appId: "1:851789126039:web:9594060ded8de271",
-    measurementId: "G-DWSR3MC0X1"
 };
 // Initialize Firebase
 
 firebase.initializeApp(firebaseConfig);
 export const db = firebase.database().ref("/");
-
-firebase.analytics.isSupported().then((isSupported) => {
-    if (isSupported) {
-        const analytics = firebase.analytics();
-    }
-})
