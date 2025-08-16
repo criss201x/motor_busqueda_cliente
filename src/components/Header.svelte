2b9664d2fe53bb4d039fc3e1af8c43f8c6827541
criss201x <cryteck_1011@hotmail.com>
2025-08-16 16:32:14 -0500

    actualizacion 2025-3

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -7,7 +7,7 @@
         </div>
         <div>
             <h2 class="texto">
-                Horarios 2025-1 Version Beta            
+                Horarios 2025-3 Version Beta            
             </h2>
         </div>
     </div>
